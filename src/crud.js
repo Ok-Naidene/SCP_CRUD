eabcc755cf4c2328c0cfa769eb1a8bc6f6ebc5c7
Ok-Naidene <30057884@student.toiohomai.ac.nz>
2023-11-03 10:27:01 +1300

    commit changes

--- src/crud.js
+++ src/crud.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { db } from './fbconfig'; 
-import { addDoc, collection, getDocs, deleteDoc, doc, updateDoc } from 'firebase/firestore'; 
+import React, { useState, useEffect, useMemo } from "react";
+import { db } from './fbconfig';
+import { addDoc, collection, getDocs, deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'; 
 
 function CRUD() {
   const [dataItem, setDataItem] = useState("");
@@ -9,16 +10,49 @@ function CRUD() {
   const [dataContainment, setDataContainment] = useState("");
   const [readData, setReadData] = useState([]);
   const [id, setId] = useState("");
-  const OurCollection = collection(db, "subjects");
+
+  // Add states for image handling
+  const [image, setImage] = useState(null);
+  const [imageURL, setImageURL] = useState("");
+
+  const OurCollection = useMemo(() => collection(db, "subjects"), []);
+
+  const handleImageChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  }
+
+  const uploadImage = async () => {
+    if (image) {
+      const storageRef = ref(getStorage(), "images/" + image.name);
+      const uploadTask = uploadBytesResumable(storageRef, image);
+
+      uploadTask.on("state_changed",
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload " + progress + "% done.");
+        },
+        (error) => {
+          console.log(error);
+        },
+        async () => {
+          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+          setImageURL(downloadURL);
+        }
+      );
+    }
+  }
 
   // CRUD Create functionality
   const crudCreate = async () => {
     try {
       await addDoc(OurCollection, {
         Item: dataItem,
-        scpClass: dataScpClass, 
+        scpClass: dataScpClass,
         description: dataDescription,
-        containment: dataContainment
+        containment: dataContainment,
+        imageURL: imageURL 
       });
 
       // Clear input fields after a successful create
@@ -26,6 +60,7 @@ function CRUD() {
       setDataClass("");
       setDataDescription("");
       setDataContainment("");
+      setImageURL(""); 
     } catch (error) {
       console.error("Error creating document:", error);
     }
@@ -44,16 +79,18 @@ function CRUD() {
       Item: dataItem,
       scpClass: dataScpClass,
       description: dataDescription,
-      containment: dataContainment
+      containment: dataContainment,
+      imageURL: imageURL 
     });
   }
 
-  const showEdit = async (id, item, scpClass, description, containment) => {
+  const showEdit = async (id, item, scpClass, description, containment, imageURL) => {
     setDataItem(item);
     setDataClass(scpClass);
     setDataDescription(description);
     setDataContainment(containment);
     setId(id);
+    setImageURL(imageURL); 
   }
 
   useEffect(() => {
@@ -67,7 +104,7 @@ function CRUD() {
       }
     }
     getData();
-  }, [OurCollection]);
+  }, []);
 
   return (
     <div className="container">
@@ -75,17 +112,23 @@ function CRUD() {
 
       <div className="input-container">
         <input className="input-field" value={dataItem} onChange={(e) => setDataItem(e.target.value)} placeholder="Item" />
-        <br />
-        <input className="input-field" value={dataScpClass} onChange={(e) => setDataClass(e.target.value)} placeholder="Class" /> 
-        <br />
+        <input className="input-field" value={dataScpClass} onChange={(e) => setDataClass(e.target.value)} placeholder="Class" />
         <input className="input-field" value={dataDescription} onChange={(e) => setDataDescription(e.target.value)} placeholder="Description" />
-        <br />
         <input className="input-field" value={dataContainment} onChange={(e) => setDataContainment(e.target.value)} placeholder="Containment" />
-        <br />
-        <button className="button" onClick={crudCreate}>Create new document</button>
-        <button className="button" onClick={crudUpdate}>Update Document</button>
-      </div>
 
+        <div className="button-container">
+          <button className="button" onClick={crudCreate}>Create new document</button>
+          <button className="button" onClick={crudUpdate}>Update Document</button>
+          <button className="button" onClick={uploadImage}>Upload Image</button>
+        </div>
+         
+        <label htmlFor="fileInput" className="btn btn-secondary black-btn">
+        <i className="fas fa-upload" style={{ color: 'black' }}></i>
+        </label>
+        <input className="form-control" type="file" id="fileInput" style={{ display: 'none' }} onChange={handleImageChange} />
+ 
+         </div>
+ 
       <div className="list-container">
         <ul>
           {readData.map((item) => (
@@ -95,9 +138,16 @@ function CRUD() {
               Description: {item.description}
               <br />
               Containment: {item.containment}
-              <div>
+
+              {item.imageURL && (
+          <div className="image-container">
+            <img className="image" src={item.imageURL} alt={item.imageURL} />
+          </div>
+        )}
+
+              <div className="button-container">
                 <button className="button" onClick={() => crudDelete(item.id)}>Delete</button>
-                <button className="button" onClick={() => showEdit(item.id, item.Item, item.scpClass, item.description, item.containment)}>Edit</button>
+                <button className="button" onClick={() => showEdit(item.id, item.Item, item.scpClass, item.description, item.containment, item.imageURL)}>Edit</button>
               </div>
             </li>
           ))}
